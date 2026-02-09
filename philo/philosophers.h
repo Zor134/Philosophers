47ca86bc7a681f617626e3fb4bd80028e7d425a9
Louis <laudinot@student.42nice.fr>
2026-02-09 15:46:46 +0100

    unsatble 1.02 joujou avec des threads

--- philo/philosophers.h
+++ philo/philosophers.h
@@ -3,40 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: louis <louis@student.42.fr>                +#+  +:+       +#+        */
+/*   By: laudinot <laudinot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 20:20:24 by louis             #+#    #+#             */
-/*   Updated: 2026/01/27 17:09:00 by louis            ###   ########.fr       */
+/*   Updated: 2026/02/09 14:38:34 by laudinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILOSOPHER_H
-#define PHILOSOPHER_H
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/time.h>
-#include <pthread.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
 
 typedef struct s_philo
 {
-    int             id;
-    int             meals_eaten;
-    long            last_meal_time;
-    pthread_t       thread;
-    struct s_data   *data;
-} t_philo;
+	int				id;
+	int				meals_eaten;	
+	long			last_meal_time;
+	pthread_t		thread;
+	struct s_data	*data;
+}	t_philo;
 
 typedef struct s_data
 {
-	int	number_of_philosophers;
-	int	time_to_die;
-	int	time_to_eat;
-	int time_to_sleep;
-	int  number_of_times_each_philosopher_must_eat;
+	int		number_of_philosophers;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		number_of_times_each_philosopher_must_eat;
+	t_philo	*philo;
+	pthread_t *philos;
 }	t_data;
-
 
 int	ft_atoi(const char *str);
 
--- philo/src/main.c
+++ philo/src/main.c
@@ -1,59 +1,109 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: louis <louis@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/26 20:20:07 by louis             #+#    #+#             */
-/*   Updated: 2026/01/27 17:08:05 by louis            ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   main.c											 :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: louis <louis@student.42.fr>				+#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2026/01/26 20:20:07 by louis			 #+#	#+#			 */
+/*   Updated: 2026/01/27 17:08:05 by louis			###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #include "../philosophers.h"
 
-int     arg_are_only_digits(char **av)
+int	arg_are_only_digits(char **av)
 {
-    int i;
-    int y;
+	int	i;
+	int	y;
 
-    i = 0;
-    while (av[++i])
-    {
-        y = -1;
-        while(av[i][++y])
-        {
-            if ((av[i][y] <= '0' || av[i][y] >= '9') && av[i][y] != '+')
-                return(printf("Please enter NUMERIC and POSITIVE arguments only.\n"));
-        }
-    }
-    return (0);
+	i = 0;
+	while (av[++i])
+	{
+		y = -1;
+		while (av[i][++y])
+		{
+			if ((av[i][y] < '0' || av[i][y] > '9') && av[i][y] != '+')
+				return (printf
+					("Please enter NUMERIC and POSITIVE arguments only.\n"));
+		}
+	}
+	return (0);
 }
 
-void    parsing(int ac, char **av, t_data *data)
+void	parsing(int ac, char **av, t_data *data)
 {
-    data->number_of_philosophers = ft_atoi(av[1]);
-    data->time_to_die = ft_atoi(av[2]);
-    data->time_to_eat = ft_atoi(av[3]);
-    data->time_to_sleep = ft_atoi(av[4]);
-    if (ac == 6)
-        data->number_of_times_each_philosopher_must_eat = ft_atoi(av[5]);
-    else
-        data->number_of_times_each_philosopher_must_eat = -1;
+	data->number_of_philosophers = ft_atoi(av[1]);
+	data->time_to_die = ft_atoi(av[2]);
+	data->time_to_eat = ft_atoi(av[3]);
+	data->time_to_sleep = ft_atoi(av[4]);
+	if (ac == 6)
+		data->number_of_times_each_philosopher_must_eat = ft_atoi(av[5]);
+	else
+		data->number_of_times_each_philosopher_must_eat = -1;
 }
 
-int main(int ac, char **av)
+void	*routine(void *arg)
 {
-    if ((ac != 5 && ac != 6 )|| arg_are_only_digits(av) != 0)
-        return (printf("How to execute : ./Philo <number_of_philosophers> <time_to_die> <time_to_eat time_to_sleep> [number_of_times_each_philosopher_must_eat]\n"));
-    t_data *data;
+	arg = NULL;
+	int	i = 0;
 
-    data = malloc(sizeof(t_data));
-    if (data == NULL)
-        return (1);
-    parsing(ac, av, data);
-    // printf("number_of_philosophers = %d\ntime_to_die = %d\ntime_to_eat = %d\ntime_to_sleep = %d\nmust_eat = %d\n", data->number_of_philosophers, data->time_to_die, data->time_to_eat, data->time_to_sleep, data->number_of_times_each_philosopher_must_eat);
-    
-    free(data);
-    return(0);
+	while (++i < 10000000)
+		;
+	printf("%d\n", i);
+	return (NULL);
+}
+
+t_philo	*init_philo(t_data *data, int id)
+{
+	t_philo	*philo;
+
+	philo = malloc(sizeof(t_philo));
+	philo->id = id;
+	philo->meals_eaten = 0;
+	philo->last_meal_time = 0;
+	philo->data = data;
+	philo->thread = 0;
+	return (philo);
+}
+
+int	init_n_philo(t_data *data)
+{
+	int		i;
+	int		t;
+	t_philo	*philo;
+
+	data->philos = malloc(sizeof(pthread_t) * data->number_of_philosophers);
+	i = -1;
+
+	while (++i < data->number_of_philosophers)
+	{
+		philo = init_philo(data, i);
+		printf("%d test\n", data->number_of_philosophers);
+		t = pthread_create(&data->philos[i], NULL, routine, NULL);
+		if (t != 0)
+			return (printf("Thread creation failed\n"));
+	}
+	// pthread_join(data->philos[0], NULL);
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	t_data			*data;
+	struct timeval	tv;
+
+	if ((ac != 5 && ac != 6) || arg_are_only_digits(av) != 0)
+		return (printf("Error, please refer to README to execute\n"));
+	data = malloc(sizeof(t_data));
+	if (data == NULL)
+		return (1);
+	parsing(ac, av, data);
+	gettimeofday(&tv, NULL);
+	printf("%ld\n", tv.tv_sec);
+	init_n_philo(data);
+	free(data);
+	// while (1)
+	// 	;
+	return (0);
 }
